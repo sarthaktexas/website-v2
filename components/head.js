f6b9e6f695efe8b6ed062a65bc43971a4f92c2bf
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-11-16 06:28:24 +0000

    🔍 add meta tags

--- components/head.js
+++ components/head.js
@@ -3,10 +3,34 @@ import Head from 'next/head';
 export default function HeadBlock() {
   return (
     <Head>
-        <title>Sarthak Mohanty</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet" />
-        <script src="/vibrate.js" />
+      <meta charset="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <title>Sarthak Mohanty</title>
+      <meta name="twitter:card" content="summary" />
+      <meta name="description" content="I'm Sarthak Mohanty, a rising high school junior and a full-stack developer and designer." />
+      <meta name="keywords" content="Sarthak, Mohanty, san, antonio, design, designer, developer, programmer, html, css, js, node, express" />
+      <meta name="author" content="Sarthak Mohanty" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@sarthaktexas" />
+      <meta name="twitter:creator" content="@sarthaktexas" />
+      <meta property="og:url" content="https://srtk.me" />
+      <meta property="og:title" content="Sarthak Mohanty" />
+      <meta property="og:description" content="I'm Sarthak Mohanty, a rising high school junior and a full-stack developer and designer." />
+      <meta property="og:image" content="https://cdn.glitch.com/52a09d93-774c-49a5-8883-9ac50e4c6dbf%2FSarthak%20Mohanty%2C%20Inc%20(1).png?v=1594723928537" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="theme-color" content="#140D1A" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="#140D1A" />
+      <meta name="apple-mobile-web-app-title" content="Sarthak Mohanty" />
+      <link rel="apple-touch-icon" href="apple-touch-icon-152x152.png" />
+      <link rel="mask-icon" href="/favicon.ico" color="#140D1A" />
+      <meta name="msapplication-TileImage" content="/msapplication-icon-144x144.png" />
+      <meta name="msapplication-TileColor" content="#140D1A" />
+      <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet" />
     </Head>
   )
 }
