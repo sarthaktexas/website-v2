88d8eeb0cad97a84f0c34ce279d45ef9eef549d0
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-11-16 06:28:02 +0000

    ✏️ Fix Typos

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function IndexPage() {
             I'm an ambitious high schooler with a keen eye for <span className="text-pink-500 underline">design</span> and I love expressing my <span className="text-purple-400 underline">creativity</span> in various ways. You can often catch me designing the school yearbook or the newest, weirdest idea that came out of my noggin.
           </h3>
           <h3 className="text-left text-white text-xl font-sans leading-normal pt-4 md:max-w-screen-sm lg:max-w-screen-sm">
-            I currently live and attend school down in the heart of the Texas hill country with my playful (and more responsible) friends. I'm a stacked AP student with an interest in going into the biomedical or creative arts fields.
+            I currently live and attend school down in the heart of the Texas hill country with my playful (and more responsible) friends. I'm a stacked AP student with an interest in going into the biomedical or creative arts field.
           </h3>
           {/* <div id="manifesto-button" className="my-4">
             <a href="/manifesto" className="bg-transparent bg-gradient-to-r hover:from-purple-400 hover:via-pink-500 hover:to-red-500 text-white font-bold hover:text-white py-2 px-2 border border-red-500 hover:border-transparent rounded">
--- pages/manifesto.js
+++ pages/manifesto.js
@@ -8,7 +8,7 @@ export default function manifestoPage() {
         <>
             <Head />
             <Nav />
-            <div className="mx-16 mt-4">
+            <div className="mx-8 md:mx-10 lg:mx-16 mt-4">
             <h2 className="text-5xl text-left text-red-400 font-bold font-sans leading-none pb-4 pt-16">
                 Pondering and Contemplation
                 <br />
@@ -53,7 +53,7 @@ export default function manifestoPage() {
                     </span>
                 </p>
             </h3>
-            <h2 className="text-2xl text-left text-blue-400 font-bold font-sans leading-none py-4">Imminent Questioning and Regret</h2>
+            <h2 className="text-2xl text-left text-blue-400 font-bold font-sans leading-none py-4">Opportunity and Regret</h2>
             <h3 className="text-left text-white text-xl font-sans leading-normal pt-4 md:max-w-screen-sm lg:max-w-screen-sm">
             <p className="story">
                 When I was in 8th grade, I had the incredible opportunity of going to National Science Bowl Finals, one which I never crossed paths with again. Of course, I didn't know this, and I <span className="text-blue-400 underline">regret</span> not spending my time enjoying the experience fully.
@@ -62,7 +62,7 @@ export default function manifestoPage() {
                 I'm a earth and space science sorta guy, but I'm also a lazy sort of guy. I could never sit down in one place and read a lengthy passage of text for a prolonged period of time. Textbooks were not my way of learning and neither were videos.
             </p>
             <p className="story">
-                I recently understood that <span className="text-blue-400">I acquired most of my knowledge from talking to teachers and subject experts and watching documentaries. I acquired the trait to lucid dream which helped me remember the tiny details in in a imagery-filled world. Those were always the interesting parts of my day.</span> It was in 8th grade where I learned how I learned. I've since applied this everywhere.
+                I recently understood that <span className="text-blue-400">I acquired most of my knowledge from talking to teachers and subject experts and watching documentaries. I learned to lucid dream which helped me remember the tiny details in in a imagery-filled world. Those were always the interesting parts of my day.</span> It was in 8th grade where I learned how I learned. I've since applied this everywhere.
             </p>
             </h3>
             <h2 className="text-2xl text-left text-purple-400 font-bold font-sans leading-none py-4">Imminent Questioning</h2>
@@ -71,22 +71,22 @@ export default function manifestoPage() {
                 Does this explain why I never got high grades? Maybe not. No, no, scratch that. Definitely not.
             </p>
             <p className="story">
-                My experienced and imaginative based learning technique has helped me learn some of the hardest topics and helped me decide some of my hardest decisions. But <span className="text-purple-400 underline">classical learning</span> has fazed me through my semi-permeable skin. I'd like to ponder about that now.
+                My experienced and imaginative based learning technique has helped me learn some of the hardest topics and helped me decide some of my hardest decisions. But <span className="text-purple-400 underline">classical learning</span> has fazed me through my semi-permeable skin.
             </p>
             <p className="story">
-                <span className="text-purple-400">At the beginning of my first full semester during the coronavirus pandemic, I've noticed a couple things. Firstly, one does not need to go to class to understand a specific subject and topic. And secondly, one does not not only need to work smarter, but also work responsibly.</span> Most of my teachers did a great job helping me manage my classwork and balance my mental health which wasn't all that difficult. Now back to being resourceful. If I was resourceful, why couldn't I handle my work as well as my fellow responsible friends?
+                <span className="text-purple-400">At the beginning of my first full semester during the coronavirus pandemic, I've noticed a couple things. Firstly, one does not need to go to class to understand a specific subject and topic. And secondly, one does not only need to work smarter, but also work responsibly.</span> Most of my teachers did a great job helping me manage my classwork and balance my mental health which wasn't all that difficult. Now back to being resourceful. If I was resourceful, why couldn't I handle my work as well as my fellow responsible friends?
             </p>
             </h3>
             <h2 className="text-2xl text-left text-pink-400 font-bold font-sans leading-none py-4">Resourcefulness</h2>
             <h3 className="text-left text-white text-xl font-sans leading-normal pt-4 md:max-w-screen-sm lg:max-w-screen-sm">
             <p className="story">
-                To me, resourcefulness doesn't mean I'm able to handle difficult situations well. To me, it means in difficult situations, I'm able to find new solutions. Abstract thought every night brings me those solutions. Nature and earthly design processes parallel those thoughts and in turn create <span className="text-pink-400 underline">new</span>, <span className="text-pink-400 underline">beautiful</span>, and <span className="text-pink-400 underline">minimal creations</span>.
+                To me, resourcefulness doesn't mean I'm able to handle difficult situations well. To me, it means in difficult situations, I'm able to find new solutions. Abstract thought every night brings me those solutions. Nature and earthly design processes parallel those thoughts and in turn create <span className="text-pink-400 underline">new</span>, <span className="text-pink-400 underline">beautiful</span>, and <span className="text-pink-400 underline">minimal</span> creations.
             </p>
             <p className="story">
                 Those creations are what my brain thinks about every little while now and then. I focus on what I think matters. I'm a software developer and designer now. <span className="text-pink-400">Those moments with Play-Doh and the abstract thoughts at night quietly metamorphosed into who I am today. Whimsical thoughts and fantastical dreams characterize how I think about topics from the maths and sciences to the creative arts and social studies. I notice the sounds around me and it inspires me to create and motivates me to finish. Ambience is the best noise for creation in my opinion and creation is what fuels thought and produces knowledge.</span>
             </p>
             <p className="story">
-                As I mold the Play-Doh of my memories into complex, three-dimensional structures, I think about what I've done to get to the point I'm at, and what I can do to become better. Whether that be the obvious or the opinionated, I strive to become more <span className="text-pink-400 underline">inventive</span> and <span className="text-pink-400 underline">resourceful</span> on my journey throughout life. So come, think and create with me!
+                As I mold the Play-Doh of my memories into complex, three-dimensional thoughts, I think about what I've done to get to the point I'm at, and what I can do to become better. Whether that be the obvious or the opinionated, I strive to become more <span className="text-pink-400 underline">inventive</span> and <span className="text-pink-400 underline">resourceful</span> on my journey throughout life. I've learned that to be heard, you must make some noise, and so starts the bang. My life is my own and the future is up to us. So go think, dream, and create. Express yourself like you've never before.
             </p>
         </h3>
                 <Signature />
