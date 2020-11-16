7a6a0dd4c6a9a244da2406291ef1fa22a85adc86
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-11-16 17:11:58 +0000

    ✏️ charset -> charSet typo

--- components/head.js
+++ components/head.js
@@ -3,7 +3,7 @@ import Head from 'next/head';
 export default function HeadBlock() {
   return (
     <Head>
-      <meta charset="utf-8" />
+      <meta charSet="utf-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
       <title>Sarthak Mohanty</title>
